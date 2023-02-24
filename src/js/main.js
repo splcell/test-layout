069210fe6a2686ffa54ef140c9e00690485a5f9b
Dmitry Kudryavtsev <splcell2013@gmail.com>
2023-02-24 16:17:14 +0700

    Delete src/js directory

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import calculator from './modules/calculator';
-import sliderStyling from './modules/sliderStyling';
-
-window.addEventListener('DOMContentLoaded', function(){
-    calculator();
-    sliderStyling();
-});
